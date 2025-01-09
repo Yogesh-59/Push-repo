97898dae16890f7e43eacf5a58cce780b7fa0bbc
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-01-09 09:58:07 +0530

    This is practice of java

---
+++ practice.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class AddTwoNum{
+ public static void main(String[]args){
+   Scanner sc=new scanner(System.in);
+   int num1=sc.nextInt();
+   int num2=sc.nextInt();
+   int ans =num1+num2;
+   System.out.println("Add two num program"+ans);
+}
+}
